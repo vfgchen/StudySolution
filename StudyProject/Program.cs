6028d20476e2b1c6bcb306eaff45c28a4ba47f43
vfgchen <vfgchen@163.com>
2025-09-23 23:43:10 +0800

    add Serilog

--- StudyProject/Program.cs
+++ StudyProject/Program.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using StudyProject.Configuration;
+using System.Text.Json;
 
 var services = new ServiceCollection();
 services.AddConfiguration();
+services.AddLogging(loggingBuilder =>
+{
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .CreateLogger();
+    loggingBuilder.AddSerilog();
+});
 var serviceProvider = services.BuildServiceProvider();
 
-var configManager = serviceProvider.GetService<ConfigManager>();
+var configManager = serviceProvider.GetRequiredService<ConfigManager>();
 var config = configManager?.GetConfig();
-Console.WriteLine(config?.Contact?.Email);
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+logger.LogDebug($"config: {JsonSerializer.Serialize(config)}");
