abf5adbc600cec06732a9c75f3809b85ea9da82b
vfgchen <vfgchen@163.com>
2025-09-25 10:44:47 +0800

    using EFCore execute raw sql

--- StudyProject/Program.cs
+++ StudyProject/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using StudyProject.Configuration;
 using StudyProject.EFCore;
 using StudyProject.Logging;
+using System.Data;
 
 var services = new ServiceCollection();
 services.AddConfiguration();
@@ -19,4 +21,31 @@ var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
 logger.LogDebug("config: {@config}", config);
 
 var ctx = serviceProvider.GetRequiredService<ApplicationDbContext>();
-ctx.Persons.ToList().ForEach(person => logger.LogInformation("person {@person}", person));
+
+var querable = ctx.Persons.FromSqlInterpolated($"select * from T_Persons where Age <= 30");
+foreach (var item in querable)
+{
+    logger.LogInformation("person : {@person}", item);
+}
+
+using (var conn = ctx.Database.GetDbConnection())
+{
+    await conn.OpenAsync();
+    var cmd = conn.CreateCommand();
+    cmd.CommandText = "select Id, Name, Age, Gender from T_Persons where Age > 30";
+    using (var reader = await cmd.ExecuteReaderAsync())
+    {
+        while (await reader.ReadAsync())
+        {
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int age = reader.GetInt32(2);
+            string gender = reader.GetString(3);
+
+            logger.LogInformation("person : {@person}", new {
+                id, name, age, gender
+            });
+        }
+    }
+}
+
